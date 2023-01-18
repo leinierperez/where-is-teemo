c7963bb0ddf738a4492639fa9fa25f8890af821f
Leinier Perez <P_leinier@yahoo.com>
2023-01-18 18:20:54 -0500

    Use levels from props

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,6 @@
 import LevelCard from '../components/LevelCard';
-import useLevels from '../hooks/useLevels';
-function Home() {
-  const [levels, isLoading, error] = useLevels();
-  // TODO: Handle loading and error
-
+import { Levels } from '../hooks/useLevels';
+function Home({ levels }: { levels: Levels }) {
   return (
     <main className="home-main py-6 px-4">
       <ul className="mx-auto grid min-h-screen max-w-screen-2xl auto-rows-max grid-cols-1 justify-items-stretch gap-6 md:grid-cols-2 xl:grid-cols-3">
