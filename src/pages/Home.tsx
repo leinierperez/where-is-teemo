ca7b0c04690e532733b32fb15b8c5a070d09926c
Leinier Perez <P_leinier@yahoo.com>
2023-01-18 12:59:07 -0500

    Change background image to only show on home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,7 +5,7 @@ function Home() {
   // TODO: Handle loading and error
 
   return (
-    <main className="py-6 px-4">
+    <main className="home-main py-6 px-4">
       <ul className="mx-auto grid min-h-screen max-w-screen-2xl auto-rows-max grid-cols-1 justify-items-stretch gap-6 md:grid-cols-2 xl:grid-cols-3">
         {levels.map((level, i) => {
           return (
