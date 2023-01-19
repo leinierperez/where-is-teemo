ba320c18082274b57e332376ef80f60878c1a937
Leinier Perez <P_leinier@yahoo.com>
2023-01-18 22:14:30 -0500

    Set up basic routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import './index.css';
+import useLevels from './hooks/useLevels';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Error from './pages/Error';
+import Root from './layouts/Root';
+
 function App() {
+  const [levels, isLoading, error] = useLevels();
+
   return (
-    <div>
-      <h1 className="text-3xl font-bold text-red-600 underline">
-        Hello world!
-      </h1>
-    </div>
+    <Routes>
+      <Route element={<Root />}>
+        <Route index element={<Home levels={levels} />}></Route>
+      </Route>
+      <Route path="*" element={<Error />}></Route>
+    </Routes>
   );
 }
 
