68480a11b9e840a357866b11d6e17d9eb2432590
Leinier Perez <P_leinier@yahoo.com>
2023-01-18 22:16:41 -0500

    Add navbar component using the time in required route and use is in Root

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,29 @@
+import teemo from '../assets/teemo.png';
+import { useParams } from 'react-router-dom';
+import Timer from './Timer';
+
+function Navbar() {
+  const { id } = useParams();
+  return (
+    <header
+      className={`bg-secondary py-1 px-4 text-lg font-bold text-primary-100 sm:text-3xl ${
+        id && 'sticky top-0 z-10'
+      }`}
+    >
+      <div className="mx-auto flex max-w-screen-2xl items-center justify-between">
+        <div className="flex items-center gap-4">
+          <img className="h-20 w-20" src={teemo} />
+          <h1 className="">Where's Teemo?</h1>
+        </div>
+        {id && <Timer />}
+        <nav>
+          <ul>
+            <li>Leaderboard</li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+}
+
+export default Navbar;
