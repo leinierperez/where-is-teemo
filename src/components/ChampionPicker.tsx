c3380226dda9d7ca49af7d74c57547cc8a73b514
Leinier Perez <P_leinier@yahoo.com>
2023-02-09 18:10:24 -0500

    Add ChampionPicker component
    The ChampionPicker component will be used to select a champion when the
    user clicks on the image. The component is dynamically positioned so
    that it doesn't go off the screen.

---
+++ src/components/ChampionPicker.tsx
@@ -0,0 +1,81 @@
+import { useLayoutEffect, useRef } from 'react';
+import { ChampionIcon } from '../hooks/useLevels';
+
+type ChampionPickerProps = {
+  icons?: ChampionIcon[];
+  clickedPosition: {
+    offsetX: number;
+    offsetY: number;
+    championX: number;
+    championY: number;
+  };
+  isChampionFound: (name: string) => void;
+  championsFound: string[];
+  levelImgRef: React.MutableRefObject<HTMLImageElement | null>;
+  navbarRef: React.MutableRefObject<HTMLElement | null>;
+};
+
+function ChampionPicker({
+  icons,
+  clickedPosition,
+  isChampionFound,
+  championsFound,
+  levelImgRef,
+  navbarRef,
+}: ChampionPickerProps) {
+  const ref = useRef<HTMLDivElement | null>(null);
+
+  useLayoutEffect(() => {
+    if (!ref.current || !levelImgRef.current || !navbarRef.current) return;
+    let left = clickedPosition.offsetX + 20;
+    let top = clickedPosition.offsetY + 20;
+    const navbarHeight = navbarRef.current.clientHeight;
+    const imageWidth = ref.current.scrollWidth;
+    const imageHeight = ref.current.scrollHeight;
+    const innerHeight = window.innerHeight;
+    const scrollY = window.scrollY;
+    const minWidth = left + imageWidth;
+    const minHeight = top + imageHeight;
+    const maxHeight = innerHeight - navbarHeight + scrollY;
+    const maxWidth = levelImgRef.current.width;
+    if (minWidth > maxWidth) {
+      left = maxWidth - imageWidth - imageWidth / 4;
+    }
+    if (minHeight > maxHeight) {
+      top =
+        innerHeight - imageHeight + scrollY - navbarHeight - imageHeight / 4;
+    }
+
+    left = (left / maxWidth) * 100;
+    top = (top / levelImgRef.current.height) * 100;
+    ref.current.style.left = `${left}%`;
+    ref.current.style.top = `${top}%`;
+  }, []);
+
+  return (
+    <>
+      <div
+        ref={ref}
+        className="absolute rounded-lg bg-secondary text-primary-100 hover:cursor-pointer"
+      >
+        <ul>
+          {icons?.map((icon, i) => {
+            if (championsFound.includes(icon.name)) return;
+            return (
+              <li
+                className="flex items-center gap-3 py-2 px-4 first:rounded-t-lg last:rounded-b-lg hover:bg-primary-100 hover:text-white"
+                key={i}
+                onClick={() => isChampionFound(icon.name)}
+              >
+                <img className="h-9 w-9" src={icon.url} />
+                <p className="">{icon.name}</p>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </>
+  );
+}
+
+export default ChampionPicker;
--- src/pages/Level.tsx
+++ src/pages/Level.tsx
@@ -1,43 +1,74 @@
 import { useParams } from 'react-router-dom';
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import useLevels from '../hooks/useLevels';
 import cursorIMG from '../assets/cursor.svg';
+import ChampionPicker from '../components/ChampionPicker';
+import { isClickPositionInChampionPosition } from '../utils';
 
-function Level({
-  isGameOver,
-  setIsGameOver,
-}: {
+type LevelProps = {
   isGameOver: boolean;
   setIsGameOver: React.Dispatch<React.SetStateAction<boolean>>;
-}) {
+  navbarRef: React.MutableRefObject<HTMLElement | null>;
+};
+
+function Level({ isGameOver, setIsGameOver, navbarRef }: LevelProps) {
   const { id } = useParams();
   const [levels] = useLevels();
   const level = levels.find((level) => level.id === Number(id));
   const [clickedPosition, setClickedPosition] = useState({
-    pageX: 0,
-    pageY: 0,
+    offsetX: 0,
+    offsetY: 0,
     championX: 0,
     championY: 0,
   });
+  const [isViewerShown, setIsViewerShown] = useState(false);
+  const [championsFound, setChampionsFound] = useState<string[]>([]);
+  const levelImgRef = useRef<HTMLImageElement | null>(null);
 
   const handleClick = (e: React.MouseEvent<HTMLImageElement, MouseEvent>) => {
-    const { pageX, pageY, offsetX, offsetY } = e.nativeEvent;
+    setIsViewerShown(!isViewerShown);
+    if (isViewerShown) return;
+    const { offsetX, offsetY } = e.nativeEvent;
     const { width, height } = e.currentTarget.getBoundingClientRect();
     const x = Math.round((offsetX / width) * 100);
     const y = Math.round((offsetY / height) * 100);
     setClickedPosition({
-      pageX: pageX,
-      pageY: pageY,
+      offsetX,
+      offsetY,
       championX: x,
       championY: y,
     });
   };
 
+  const isChampionFound = (name: string) => {
+    const targetChampion = level?.championPositions.find(
+      (pos) => pos.name === name
+    );
+    if (
+      targetChampion &&
+      isClickPositionInChampionPosition(clickedPosition, targetChampion)
+    ) {
+      setChampionsFound([...championsFound, name]);
+    }
+    setIsViewerShown(false);
+  };
+
   return (
     <main className="min-h-screen w-full">
       <div className="relative flex justify-center">
+        {isViewerShown && (
+          <ChampionPicker
+            icons={level?.championIcons}
+            clickedPosition={clickedPosition}
+            isChampionFound={isChampionFound}
+            championsFound={championsFound}
+            levelImgRef={levelImgRef}
+            navbarRef={navbarRef}
+          />
+        )}
         <img
           src={level?.imageURL}
+          ref={levelImgRef}
           className="w-full"
           style={{
             cursor: `url('${cursorIMG}') 27 27, auto`,
