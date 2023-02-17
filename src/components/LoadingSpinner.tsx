acc64d1a7c01f5270f96791a63912b14ee2923af
Leinier Perez <P_leinier@yahoo.com>
2023-02-16 23:11:12 -0500

    Add LoadingSpinner component to handle loading states

---
+++ src/components/LoadingSpinner.tsx
@@ -0,0 +1,7 @@
+function LoadingSpinner() {
+  return (
+    <div className="h-16 w-16 animate-spin rounded-full border-8 border-primary-100 border-t-gray-500" />
+  );
+}
+
+export default LoadingSpinner;
